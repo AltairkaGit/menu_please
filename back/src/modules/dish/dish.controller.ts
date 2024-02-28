ff9cd88cf89f6a88580718382cc665bbe2493d9a
Кирилл Евгеньевич <kriv466@gmail.com>
2024-02-28 11:00:00 +0300

    reoles guard creating

--- back/src/modules/dish/dish.controller.ts
+++ back/src/modules/dish/dish.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { DishService } from './dish.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateDishFormDto } from './dto';
+import { JwtGuard } from '@src/guards/jwt-guard';
+import { Roles } from '@src/guards/roles.decorator';
+import { Role } from '@src/guards/role.enum';
+import { RolesGuard } from '@src/guards/roles-guard';
 
 @Controller('dish')
 export class DishController {
     constructor(private readonly dishService: DishService) {}
 
-
     @Post()
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles(Role.Cooker)
     @UseInterceptors(FileInterceptor('file'))
     createDish(@UploadedFile(
         new ParseFilePipeBuilder()
