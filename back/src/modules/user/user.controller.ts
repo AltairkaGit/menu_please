dff4a4879647d297c5955dd5fbb42ed3f5fbc65f
Кирилл Евгеньевич <kriv466@gmail.com>
2024-03-09 15:36:58 +0300

    getting user data by cookie

--- back/src/modules/user/user.controller.ts
+++ back/src/modules/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { CreateUserDto } from "./dto";
+import { JwtGuard } from "@src/guards/jwt-guard";
+import { UserDto } from "../auth/dto";
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Get()
+    @UseGuards(JwtGuard)
+    async retreiveUser(@Req() req: any) {
+        const { user: imposter } = req;
+        const user = await this.userService.findUserById(imposter.userId)
+        return new UserDto(user)
+    }
 
 }
